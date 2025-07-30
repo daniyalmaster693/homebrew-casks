5557ae9f748d57f3dbf11fc8073fb703c7d8eb51
Daniyal Master <daniyalmaster693@gmail.com>
2025-07-30 12:33:30 -0400

    Updated supercorners.rb

--- Casks/supercorners.rb
+++ Casks/supercorners.rb
@@ -1,6 +1,6 @@
 cask "supercorners" do
   version "1.3.1"
-  sha256 "06221f6c8a78cdad24abff9183cc4485c2e5d7853df65ddd4a7d0c6c3b1d0f79"
+  sha256 "c47aba923f67c96b567e42dec1c77b2fe5b65da30918cfc4aacab958478e75f9"
 
   url "https://github.com/daniyalmaster693/SuperCorners/releases/download/1.3.1/SuperCorners.zip"
   name "SuperCorners"
