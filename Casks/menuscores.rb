70752a01a981b157a1f7e8885098227c81c3ebd2
Daniyal Master <daniyalmaster693@gmail.com>
2026-06-27 18:18:34 -0400

    Updated MenuScores cask

--- Casks/menuscores.rb
+++ Casks/menuscores.rb
@@ -2,7 +2,7 @@ cask "menuscores" do
   version "2.1.3"
   sha256 "9a3032e5517907a3c8c9bc3c2e07d9fb614d6f96341562d2a46b022177b263d2"
 
-  url "https://github.com/daniyalmaster693/MenuScores/releases/download/2.1.3/MenuScores.app.zip"
+  url "https://github.com/daniyalmaster693/MenuScores/releases/download/2.1.3/MenuScores.zip"
   name "MenuScores"
   desc "Live Scores - Right From Your Notch"
   homepage "https://menuscores.vercel.app/"
